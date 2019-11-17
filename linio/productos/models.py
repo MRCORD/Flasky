c7a63cfd8bde00f73d7aa55ee311bc2cf38c35c7
oscar javier Rivas Delgado <oj.rivasd@alum.up.edu.pe>
2019-11-16 19:03:23 -0500

    Update models.py

--- linio/productos/models.py
+++ linio/productos/models.py
@@ -13,4 +13,15 @@ class Comercio(db.Model):
     distrito = db.Column(db.String(2), unique=False, nullable=False)
     direccion = db.Column(db.String(40), unique=False, nullable=False)
 
+class Producto(db.Model):
+   id =  db.Column(db.Integer, primary_key = True)
+   nombre = db.Column(db.String(30), unique=True, nullable=False)
+   precio = db.Column(db.Numeric(10,2), nullable=False)
+   descripcion = db.Column(db.String(300), unique=False, nullable=False)
+   comercio_id = db.Column(db.Integer, db.ForeignKey('comercio.id'), nullable=False)
+   comercio = db.relationship('Comercio', backref=db.backref('comercio'))
+   categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+   comercio = db.relationship('Categoria', backref=db.backref('categoria'))
+
+
 db.create_all()
